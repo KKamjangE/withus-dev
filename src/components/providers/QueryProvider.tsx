1e2ec12e177d2773b698b3d4396e353f9ccd2cdd
KKamjangE <ajm980718@gmail.com>
2024-01-15 14:09:05 +0900

    Feat: query provider 구현

---
+++ src/components/providers/QueryProvider.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+interface QueryProviderProps {
+    children: ReactNode;
+}
+
+const QueryProvider = ({ children }: QueryProviderProps) => {
+    const queryClient = new QueryClient();
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    );
+};
+
+export default QueryProvider;
