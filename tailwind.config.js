4529c7d798a654f97c10353c095863fc6b34c83b
KkamjangE <ajm980718@gmail.com>
2024-04-04 17:51:35 +0900

    Chore: tailwindcss init

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./index.html', '*./src/**/*.{tsxm,jsx,ts,js,html}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
