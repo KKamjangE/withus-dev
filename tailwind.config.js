479f5abc5bb0c78a0d50348bad44e32a910640ce
KkamjangE <ajm980718@gmail.com>
2024-04-05 11:05:30 +0900

    Feat: tailwind daisyui 플러그인 및 테마 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', '*./src/**/*.{tsxm,jsx,ts,js,html}'],
+    content: ['./src/**/*.{tsx,jsx,ts,js,html}'],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [require('daisyui')],
+    daisyui: {
+        themes: ['bumblebee'],
+        darkTheme: 'business',
+    },
 };
