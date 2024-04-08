bdf10ebf529eaeeff6bc25092ea60be68fcae136
KkamjangE <ajm980718@gmail.com>
2024-04-08 10:05:52 +0900

    Style: 모바일 사이즈로 변경

--- src/page/Auth/AuthLayout.tsx
+++ src/page/Auth/AuthLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 const AuthLayout = () => {
     return (
-        <div className="max-w-screen-md mx-auto">
+        <div className="max-w-screen-sm mx-auto py-10">
             <Outlet />
         </div>
     );
--- src/page/Auth/index.tsx
+++ src/page/Auth/index.tsx
@@ -10,16 +10,18 @@ import Input from '#/common/components/Input';
 const AuthPage = () => {
     const { mutate } = usePostSignIn();
     return (
-        <div className="flex flex-col gap-6 items-center mt-28">
-            <CloudIcon className="h-36 w-36 text-cyan-500" />
-            <h2 className="text-5xl font-bold">
+        <div className="flex flex-col gap-4 items-center">
+            <CloudIcon className="size-24 text-cyan-500" />
+            <h2 className="text-2xl font-bold">
                 WithUS에 오신 것을 환영합니다!
             </h2>
-            <h2 className="text-xl">
-                <span className="font-bold">그룹에서 사용하는 이메일 주소</span>
+            <h2 className="text-lg text-center">
+                <span className="font-bold block">
+                    그룹에서 사용하는 이메일 주소
+                </span>
                 로 로그인하는 것을 추천드려요.
             </h2>
-            <form className="flex flex-col items-center gap-3 w-full max-w-sm">
+            <form className="flex flex-col items-center gap-3 w-full max-w-80">
                 <Input
                     forwardIcon={<EnvelopeIcon width={18} />}
                     placeholder="name@work-email.com"
@@ -37,13 +39,15 @@ const AuthPage = () => {
                 />
                 <Button
                     type="submit"
-                    className="btn-neutral text-lg font-medium"
+                    className="btn-neutral text-lg font-medium text-white"
                 >
                     시작
                 </Button>
             </form>
-            <span className="divider">또는</span>
-            <Button className="btn-outline text-lg font-medium max-w-sm flex items-center hover:bg-white hover:text-black">
+            <div className="w-full max-w-80">
+                <span className="divider">또는</span>
+            </div>
+            <Button className="btn-outline text-lg font-medium flex items-center max-w-80 hover:bg-white hover:text-black">
                 <GoogleLogo width={30} height={30} />
                 sign with google
             </Button>
