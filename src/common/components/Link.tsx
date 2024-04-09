5dc9529b99b9c01f60b3fcc2c73d00ccc971192f
KkamjangE <ajm980718@gmail.com>
2024-04-09 14:35:50 +0900

    Feat: 공통 컴포넌트 theme 색상 적용

--- src/common/components/Link.tsx
+++ src/common/components/Link.tsx
@@ -4,7 +4,7 @@ const Link = ({ to, className, children, ...rest }: LinkProps) => {
     return (
         <LinkBase
             to={to}
-            className={`text-blue-500 underline-offset-2 hover:underline ${className}`}
+            className={`font-normal text-primary underline-offset-2 hover:underline ${className}`}
             {...rest}
         >
             {children}
