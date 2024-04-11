33886761e10893d69fa7f56a8bf30545cd9f2372
KkamjangE <ajm980718@gmail.com>
2024-04-11 16:49:10 +0900

    Feat: main layout 컴포넌트 구현

--- src/Layout/MainLayout.tsx
+++ src/Layout/MainLayout.tsx
@@ -1,11 +1,13 @@
-import GroupSidebar from '#/Layout/navigation/GroupSidebar';
+import SideMenu from '#/layout/SideMenu';
 import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
     return (
         <div className="relative">
-            <GroupSidebar />
-            <Outlet />
+            <SideMenu />
+            <div className="bg-neutral">
+                <Outlet />
+            </div>
         </div>
     );
 };
