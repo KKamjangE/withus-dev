b2fe5c28c7ae69490449858da354d0f2b6ba1aad
KkamjangE <ajm980718@gmail.com>
2024-04-11 16:47:50 +0900

    Feat: group list component 구현

---
+++ src/Layout/SideMenu/GroupList.tsx
@@ -0,0 +1,26 @@
+import GroupItem from '#/layout/SideMenu/GroupItem';
+import { SquaresPlusIcon } from '@heroicons/react/24/outline';
+
+const GroupList = () => {
+    const groupList = [
+        { groupName: 'A', id: '1' },
+        { groupName: 'B', id: '2' },
+        { groupName: 'C', id: '3' },
+    ];
+
+    return (
+        <div className="flex h-screen flex-col items-center bg-base-300">
+            {/* <h1 className="px-3 py-5 text-xl font-semibold">WidthUS</h1> */}
+            <div className="mt-3 flex grow flex-col gap-5 p-3">
+                {groupList.map((item, index) => (
+                    <GroupItem key={index} groupItem={item} />
+                ))}
+                <button className="btn btn-square">
+                    <SquaresPlusIcon />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default GroupList;
