c0f318d74f50a7f8cdd08269d5011f109e9a7d56
KkamjangE <ajm980718@gmail.com>
2024-04-11 16:47:30 +0900

    Feat: group item component 생성

--- src/Layout/navigation/GroupSidebar/GroupItem.tsx
+++
@@ -1,9 +0,0 @@
-const GroupItem = ({ groupName }: { groupName: string }) => {
-    return (
-        <button className="btn size-16 btn-circle btn-primary text-2xl text-secondary-content">
-            {groupName}
-        </button>
-    );
-};
-
-export default GroupItem;
