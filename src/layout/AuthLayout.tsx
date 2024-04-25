2c716ca4a7daa11cc3ad7a8da49d32d510d1eec1
KkamjangE <ajm980718@gmail.com>
2024-04-25 13:24:06 +0900

    Rename: 폴더명 수정

---
+++ src/layout/AuthLayout.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
+const AuthLayout = () => {
+    return (
+        <div className="mx-auto max-w-screen-sm py-10 md:py-20">
+            <Outlet />
+        </div>
+    );
+};
+
+export default AuthLayout;
