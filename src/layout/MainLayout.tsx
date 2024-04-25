e8ff65378ce51087e52af6ac419958ec25514c12
KkamjangE <ajm980718@gmail.com>
2024-04-25 18:02:48 +0900

    Style: main layout style 수정

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -6,9 +6,10 @@ const MainLayout = () => {
     return (
         <div>
             <SideMenu />
-            <div className='fixed flex h-10 w-screen items-center justify-between bg-base-300 px-2'>
+            <div className='fixed z-10 flex h-10 w-screen items-center justify-between bg-base-300 px-2'>
                 <h1 className='text-base font-semibold'>WithUS</h1>
             </div>
+            <div className='h-10' />
             <div className='ml-[300px] h-full bg-neutral'>
                 <Outlet />
             </div>
