329a66d71868a2253d682e9151912daf1b9472fd
KKamjangE <ajm980718@gmail.com>
2024-04-26 15:55:44 +0900

    Style: 코드 포맷팅

--- src/Layout/AuthLayout.tsx
+++ src/Layout/AuthLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 const AuthLayout = () => {
     return (
-        <div className='mx-auto max-w-screen-sm py-10 md:py-20'>
+        <div className="mx-auto max-w-screen-sm py-10 md:py-20">
             <Outlet />
         </div>
     );
