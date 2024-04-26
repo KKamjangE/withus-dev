a84943be63040c42026ea2a6db6da1903599c35f
KkamjangE <ajm980718@gmail.com>
2024-04-26 16:04:15 +0900

    Style: 코드 포맷팅

--- src/Layout/SideMenu/GroupMenu.tsx
+++ src/Layout/SideMenu/GroupMenu.tsx
@@ -5,18 +5,18 @@ import GroupMenuItem from '#/layout/SideMenu/GroupMenuItem';
 const GroupMenu = () => {
     const [selectedMenu, setSelectedMenu] = useState(null);
     return (
-        <div className='bg-base-300'>
-            <div className='flex h-screen w-56 flex-col rounded-tl-2xl bg-base-100 py-5 align-middle'>
-                <nav className='flex flex-col gap-2'>
-                    <h3 className='m-3 text-xl'>✨ Project</h3>
+        <div className="bg-base-300">
+            <div className="flex h-screen w-56 flex-col rounded-tl-2xl bg-base-100 py-5 align-middle">
+                <nav className="flex flex-col gap-2">
+                    <h3 className="m-3 text-xl">✨ Project</h3>
                     <GroupMenuItem to={'/'}>피드</GroupMenuItem>
                     <GroupMenuItem to={'/'}>칸반보드</GroupMenuItem>
                     <GroupMenuItem to={'/'}>캘린더</GroupMenuItem>
                     <GroupMenuItem to={'/'}>뭐시기</GroupMenuItem>
                 </nav>
-                <span className='divider' />
-                <nav className='flex flex-col gap-2'>
-                    <h3 className='m-3 text-xl'>💬 Chat</h3>
+                <span className="divider" />
+                <nav className="flex flex-col gap-2">
+                    <h3 className="m-3 text-xl">💬 Chat</h3>
                     <GroupMenuItem to={'/'}>김씨</GroupMenuItem>
                     <GroupMenuItem to={'/'}>박씨</GroupMenuItem>
                     <GroupMenuItem to={'/'}>이씨</GroupMenuItem>
